6e7ad18acacd05c931cc4275faecd186cc2c0237
PastaG1903 <mallito1903@hotmail.com>
2024-12-08 13:33:35 -0600

    Adding p_cap in progress

--- run_cuf_simulink.m
+++ run_cuf_simulink.m
@@ -1,5 +1,7 @@
-load_system('./C_uF_lib.slx');
-load_system('./untitled.slx');
+% START OF THE CL SCRIPT
+
+load_system('C_uF_lib.slx');
+load_system('untitled.slx');
 
 % set_param("untitled","DataLogging","on");
 % set_param('./C_uF_lumped_MATLAB/untitled.slx/mem_pneum_res1.ssc','mem_rad','5');
@@ -11,11 +13,12 @@ set_param('untitled','FixedStep','0.01')
 set_param('untitled/Signal Editor','Filename','./Velocity profiles/10-10.mat')
 
 % VENTED CHAMBER
-set_param('untitled/vented_chamber','init_fill','1')
+set_param('untitled/vented_chamber','init_fill','0.99')
 set_param('untitled/vented_chamber','d','4.04')
 set_param('untitled/vented_chamber','anglength','160')
 set_param('untitled/vented_chamber','InRad','15')
 set_param('untitled/vented_chamber','OutRad','45')
+set_param('untitled/vented_chamber','theta','pi/4')
 
 % RADIAL CHANNEL
 set_param('untitled/n_radial_channel','w','0.5') % in mm
@@ -38,14 +41,14 @@ set_param('untitled/n_isoradial_channel','dir','-1')
 set_param('untitled/mem_pneum_res1','init_fill','0')
 set_param('untitled/mem_pneum_res1','d','4.04')
 set_param('untitled/mem_pneum_res1','anglength','160')
-set_param('untitled/mem_pneum_res1','InRad','15')
-set_param('untitled/mem_pneum_res1','OutRad','45')
-set_param('untitled/mem_pneum_res1','mem_rad','19')
-set_param('untitled/mem_pneum_res1','mem_thick','50')
-set_param('untitled/mem_pneum_res1','init_stress','0')
+set_param('untitled/mem_pneum_res1','InRad','45')
+set_param('untitled/mem_pneum_res1','OutRad','62')
+set_param('untitled/mem_pneum_res1','mem_rad','26')
+set_param('untitled/mem_pneum_res1','mem_thick','80')
+set_param('untitled/mem_pneum_res1','init_stress','125000')
 set_param('untitled/mem_pneum_res1','theta','pi/4')
-set_param('untitled/mem_pneum_res1','Young','2000000')
-set_param('untitled/mem_pneum_res1','Poisson','0.5')
+set_param('untitled/mem_pneum_res1','Young','6000000')
+set_param('untitled/mem_pneum_res1','Poisson','0.63')
 
 
 simOut = sim('untitled.slx');
@@ -54,5 +57,5 @@ runnn = Simulink.sdi.getAllRunIDs();
 % runID = Simulink.sdi.getRun(runCount);
 Simulink.sdi.exportRun(runnn(end),'to','file','filename','sdi_export.xlsx');
 %export(runnn(runCount));
-close_system('./C_uF_lib.slx',0);
-close_system('./untitled.slx',0);
+close_system('C_uF_lib.slx',1);
+close_system('untitled.slx',1);
