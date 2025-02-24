dd138e32fa078c0015b5d692cedbafe552ffb3b3
PastaG1903 <mallito1903@hotmail.com>
2025-02-24 16:08:32 -0600

    Script update

--- run_cuf_smlk_mm.m
+++ run_cuf_smlk_mm.m
@@ -8,7 +8,7 @@ load_system('titled.slx');
 
 set_param('titled','Decimation','1000000');
 set_param('titled','FixedStep','0.0005');
-set_param('titled','StopTime','20');
+set_param('titled','StopTime','30');
 set_param('titled/Signal Editor','Filename','/mnt/flashMich/shay/C_uF_lumped_MATLAB/Velocity profiles/real_repro.mat');
 
 % set_param('titled/Signal Editor','OutputAfterFinalValue','Extrapolation');
@@ -24,6 +24,7 @@ set_param('titled/vented_chamber','anglength','160');
 set_param('titled/vented_chamber','InRad','15');
 set_param('titled/vented_chamber','OutRad','45');
 set_param('titled/vented_chamber','theta','pi/2');
+set_param('titled/vented_chamber','G','-1'); % -1 when outlet points against direction of rotation
 
 % RADIAL CHANNEL
 set_param('titled/n_radial_channel','w','0.475'); % in mm
@@ -40,7 +41,7 @@ set_param('titled/n_isoradial_channel','anglength','20');
 set_param('titled/n_isoradial_channel','r','62');
 set_param('titled/n_isoradial_channel','init_fill','0');
 set_param('titled/n_isoradial_channel','theta','pi/4');
-set_param('titled/n_isoradial_channel','dir','-1');
+set_param('titled/n_isoradial_channel','dir','-1'); %-1 when the forward flow goes against the direction of rotation
 
 % MEM PNEUM CHAMBER
 set_param('titled/multi_mem_res','init_fill','0');
@@ -49,6 +50,7 @@ set_param('titled/multi_mem_res','anglength','160'); %°
 set_param('titled/multi_mem_res','InRad','45'); %mm
 set_param('titled/multi_mem_res','OutRad','62'); %mm
 set_param('titled/multi_mem_res','theta','pi/2');
+set_param('titled/multi_mem_res','G','1'); %1 when flow is opposite to the direction of rotation
 
 %   Membrane 1
 set_param('titled/multi_mem_res','mem_rad_1','21'); %mm
@@ -78,6 +80,6 @@ runnn = Simulink.sdi.getAllRunIDs();
 % runID = Simulink.sdi.getRun(runCount);
 Simulink.sdi.exportRun(runnn(end),'to','file','filename','sdi_export.xlsx');
 %export(runnn(runCount));
-save_system('titled.slx',1);
+save_system('titled.slx');
 close_system('titled.slx',1);
 close_system('C_uF_lib.slx',0);
