add25d2eb9cf3e5e4c3a0550bf15729b7f518e13
PastaG1903 <mallito1903@hotmail.com>
2025-06-25 22:03:38 -0600

    Added both pneum and mem equations to single block

--- run_cuf_simulink.m
+++ run_cuf_simulink.m
@@ -2,18 +2,18 @@
 
 load_system('C_uF_lib.slx');
 load_system('untitled_repl.slx');
-
+% load_system('repl_nomem.slx');
 
 % MODEL PARAMETERS
 
 set_param('untitled_repl','Decimation','100000');
 set_param('untitled_repl','FixedStep','0.001');
-set_param('untitled_repl','StopTime','110');
+set_param('untitled_repl','StopTime','120');
 set_param('untitled_repl/Signal Editor','Filename','/home/shay/FM_SSD/shay/C_uF_lumped_MATLAB/Velocity profiles/100_100_1000.mat');
 
 % set_param('untitled_repl/Signal Editor','OutputAfterFinalValue','Extrapolation');
-% set_param('untitled_repl/Signal Editor','OutputAfterFinalValue','Holding final value');
-set_param('untitled_repl/Signal Editor','OutputAfterFinalValue','Setting to zero');
+set_param('untitled_repl/Signal Editor','OutputAfterFinalValue','Holding final value');
+% set_param('untitled_repl/Signal Editor','OutputAfterFinalValue','Setting to zero');
 
 set_param('untitled_repl/Signal Editor','SampleTime','0.001');
 
@@ -26,11 +26,11 @@ set_param('untitled_repl/vented_chamber_s','OutRad','31.5');
 set_param('untitled_repl/vented_chamber_s','theta','pi/2');
 
 % RADIAL CHANNEL
-set_param('untitled_repl/n_radial_channel','w','0.745'); % in mm
+set_param('untitled_repl/n_radial_channel','w','2'); % in mm
 set_param('untitled_repl/n_radial_channel','d','2'); % in mm
-set_param('untitled_repl/n_radial_channel','r_in','25.5');
-set_param('untitled_repl/n_radial_channel','r_out','36');
-set_param('untitled_repl/n_radial_channel','init_vol','500');
+set_param('untitled_repl/n_radial_channel','r_in','31.5');
+set_param('untitled_repl/n_radial_channel','r_out','81.5');
+set_param('untitled_repl/n_radial_channel','init_vol','200');
 set_param('untitled_repl/n_radial_channel','theta','pi/2');
 
 % ISORADIAL CHANNEL
@@ -45,17 +45,25 @@ set_param('untitled_repl/n_radial_channel','theta','pi/2');
 % MEM PNEUM CHAMBER
 set_param('untitled_repl/mem_pneum_res_s','init_vol','0');
 set_param('untitled_repl/mem_pneum_res_s','d','2'); %mm
-set_param('untitled_repl/mem_pneum_res_s','w','3.06'); %mm
-set_param('untitled_repl/mem_pneum_res_s','InRad','25.5'); %mm
-set_param('untitled_repl/mem_pneum_res_s','OutRad','36'); %mm
-set_param('untitled_repl/mem_pneum_res_s','mem_rad','3'); %mm
-set_param('untitled_repl/mem_pneum_res_s','mem_thick','220'); %um THIS IS THE ONLY VALUE IN MICRONS
-set_param('untitled_repl/mem_pneum_res_s','init_stress','58000'); %Pa
-set_param('untitled_repl/mem_pneum_res_s','Young','1200000'); %Pa
-set_param('untitled_repl/mem_pneum_res_s','Poisson','0.48');
+set_param('untitled_repl/mem_pneum_res_s','w','5'); %mm
+set_param('untitled_repl/mem_pneum_res_s','InRad','31.5'); %mm
+set_param('untitled_repl/mem_pneum_res_s','OutRad','81.5'); %mm
+set_param('untitled_repl/mem_pneum_res_s','mem_rad','5'); %mm
+set_param('untitled_repl/mem_pneum_res_s','mem_thick','100'); %um THIS IS THE ONLY VALUE IN MICRONS
+set_param('untitled_repl/mem_pneum_res_s','init_stress','0'); %Pa
+set_param('untitled_repl/mem_pneum_res_s','Young','9800000'); %Pa
+set_param('untitled_repl/mem_pneum_res_s','Poisson','0.01');
 set_param('untitled_repl/mem_pneum_res_s','theta','pi/2');
 
-simout = sim('untitled_repl.slx');
+% VENTED CHAMBER AT THE END
+% set_param('untitled_repl/vent_end_chamber','init_vol','0');
+% set_param('untitled_repl/vent_end_chamber','d','2');
+% set_param('untitled_repl/vent_end_chamber','w','5');
+% set_param('untitled_repl/vent_end_chamber','InRad','31.5');
+% set_param('untitled_repl/vent_end_chamber','OutRad','81.5');
+% set_param('untitled_repl/vent_end_chamber','theta','pi/2');
+
+simout = sim('./untitled_repl.slx');
 % runCount = Simulink.sdi.getRunCount();
 runnn = Simulink.sdi.getAllRunIDs;
 % runID = Simulink.sdi.getRun(runCount);
